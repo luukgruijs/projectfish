bb4eb7728cd3bba8bed0c68a76ae1e2823cbd28f
luuk gruijs <luukgruijs@luuks-MacBook-Pro.local>
2018-10-17 22:11:08 +0200

    first step in moving towards serverless

---
+++ api/models/schemas/LunchOrder.js
@@ -0,0 +1,18 @@
+"user strict"
+
+module.exports = function Order(mongoose) {
+    var schema = new mongoose.Schema({
+        "created_at": {
+            "default": new Date(),
+            "desc": "The date at which the lunch order is created",
+            "type": Date
+        },
+        "orders": [{
+            "desc": "A list of order ids attached to this lunch order",
+            "ref": "Order",
+            "type": mongoose.Schema.Types.ObjectId
+        }]
+    })
+
+    return mongoose.model("LunchOrder", schema)
+}
---
+++ api/models/schemas/Order.js
@@ -0,0 +1,48 @@
+"user strict"
+
+module.exports = function Order(mongoose) {
+    var schema = new mongoose.Schema({
+        "amount": {
+            "desc": "The total basket price",
+            "required": true,
+            "type": Number
+        },
+        "created_at": {
+            "default": new Date(),
+            "desc": "The date at which the basket is created",
+            "type": Date
+        },
+        "items": {
+            "required": true,
+            "desc": "The items in an order",
+            "type": [{
+                "name": {
+                    "desc": "The name of the item",
+                    "required": true,
+                    "trim": true,
+                    "type": String
+                },
+                "category": {
+                    "desc": "The category of the item",
+                    "type": String
+                },
+                "price": {
+                    "desc": "The price of the item",
+                    "required": true,
+                    "type": Number,
+                },
+                "quantity":{
+                    "type": Number,
+                    "default": 1,
+                    "required": true
+                }
+            }]
+        },
+        "user": {
+            "desc": "The ID of the user that orders the items",
+            "type": mongoose.Schema.Types.ObjectId
+        }
+    })
+
+    return mongoose.model("Order", schema)
+}
